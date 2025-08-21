e079384ecf6f5929acac6678729e713ffce01a71
hathor2k <hathor2k@naver.com>
2025-08-21 16:27:40 +0900

    add jwt filter

--- flux-apigateway-service/src/main/java/com/example/fluxapigatewayservice/filter/AuthorizationHeaderFilter.java
+++ flux-apigateway-service/src/main/java/com/example/fluxapigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -1,4 +1,85 @@
 package com.example.fluxapigatewayservice.filter;
 
-public class AuthorizationHeaderFilter extends LoggingFilter {
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+    Environment env;
+
+    public AuthorizationHeaderFilter(Environment env) {
+        super(Config.class);
+        this.env = env;
+    }
+
+    public static class Config {
+        // put configuration properties here
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> { ServerHttpRequest request = exchange.getRequest();
+
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return OnError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer ", "");
+
+            if(!isJwtValid(jwt)) {
+                return OnError(exchange, "Invalid JWT", HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> OnError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        log.error(err);
+
+        byte[] bytes = "The requested token is invalid".getBytes(StandardCharsets.UTF_8);
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+
+        return response.writeWith(Flux.just(buffer));
+    }
+
+    private boolean isJwtValid(String jwt) {
+        byte[] secretKeyBytes = env.getProperty("token.secret").getBytes();
+        SecretKeySpec signingKey = new SecretKeySpec(secretKeyBytes, io.jsonwebtoken.SignatureAlgorithm.HS512.getJcaName());
+        boolean returnValue = true;
+        String subject = null;
+
+        try {
+            JwtParser jwtParser = Jwts.parser().setSigningKey(signingKey).build();
+            subject = jwtParser.parseClaimsJws(jwt).getBody().getSubject();
+        } catch (Exception e) {
+            returnValue = false;
+        }
+
+        if(subject == null || subject.isEmpty()) {
+            returnValue = false;
+        }
+
+        return returnValue;
+    }
 }
